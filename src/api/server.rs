a5c8f81792ead75009b000353c3f6bb8b7e999b6
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2024-05-13 14:59:57 -0300

    feat: update deploy and log

--- src/api/server.rs
+++ src/api/server.rs
@@ -45,7 +45,6 @@ pub async fn main() -> Result<(), rocket::Error> {
     let jwt_authentication_adapter: Arc<dyn AuthenticationAdapter + Sync + Send> =
         Arc::new(JWTAuthenticationAdapter::new(config.secret.clone()));
 
-    println!("TESTEEEEEEEEEE222222");
     println!("Loading environment variables...");
     let usuario_repository: Arc<Mutex<dyn UsuarioGateway + Sync + Send>> = if config.env
         == Env::Test
